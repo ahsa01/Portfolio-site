04f18d3838b02ae26825fcfabdead9ddb982bbae
ahsa01 <133399515+ahsa01@users.noreply.github.com>
2025-02-25 01:37:22 +0530

    Update project.js

--- SOURCE/components/projects/project.js
+++ SOURCE/components/projects/project.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './project.css';
 import icn1 from '../../assets/e-Learning.jpg';
@@ -7,46 +8,47 @@ import icn3 from '../../assets/tic tac toe.jpg';
 const Project = () => {
   return (
     <div>
-        <section class="courses" id="project">
+        <section className="courses" id="project">
             <h2>My Projects</h2>
-            <div class="container courses_container">
-                <article class="course">
+            <div className="container courses_container">
+                <article className="course">
                     <div>
-                        <img src={icn1} alt="" class="course_image" />
+                        <img src={icn1} alt="E Learning Website" className="course_image" />
                     </div>
-                    <div class="course_info">
-                        <h4>E Learning website</h4>
-                        
-                        <a href="courses.html" class="btn btn-primary">Learn More</a>
+                    <div className="course_info">
+                        <h4>E Learning Website</h4>
+                        <a href="https://github.com/ahsa01/E-Learning-site" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                            View on GitHub
+                        </a>
                     </div>
                 </article>
 
-                <article class="course">
-                    <div >
-                        <img
-                            src={icn2} alt="" class="course_image"/>
+                
+
+                <article className="course">
+                    <div>
+                        <img src={icn3} alt="Object-Detection-with-Voice-Feedback-YOLO-v3" className="course_image"/>
                     </div>
-                    <div class="course_info">
+                    <div className="course_info">
+                        <h4>Tic Tac Toe Game</h4>
+                        <a href="https://github.com/ahsa01/Object-Detection-with-Voice-Feedback-YOLO-v3" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                            View on GitHub
+                        </a>
+                    </div>
+                </article>
+               <article className="course">
+                    <div>
+                        <img src={icn2} alt="Portfolio" className="course_image"/>
+                    </div>
+                    <div className="course_info">
                         <h4>Portfolio</h4>
-                        
-                        <a href="courses.html" class="btn btn-primary">Learn More</a>
-                    </div>
-                </article>
-
-                <article class="course">
-                    <div >
-                        <img src={icn3} alt="" class="course_image"/>
-                    </div>
-                    <div class="course_info">
-                        <h4>TIC TAC TOE Game</h4>
-                       <br></br>
-                        <a href="courses.html" class="btn btn-primary">Learn More</a>
+                        <a href="https://github.com/ahsa01/Portfolio-site" className="btn btn-primary">Learn More</a>
                     </div>
                 </article>
             </div>
         </section>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
